1e87d63c8ed048ebec2446aff5d4a1b221da5fb1
aelkhodary <40409487+aelkhodary@users.noreply.github.com>
2020-04-29 02:33:23 +0400

    elastic search cliet

--- kafka-consumer-elasticsearch/src/main/java/com/kafka/api/search/ElasticSearchConsumer.java
+++ kafka-consumer-elasticsearch/src/main/java/com/kafka/api/search/ElasticSearchConsumer.java
@@ -1,4 +1,59 @@
 package com.kafka.api.search;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.*;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class ElasticSearchConsumer {
+
+    //elasticSearch client
+    public static RestHighLevelClient createClient() {
+        String hostName = "";
+        String userName = "";
+        String passWord = "";
+
+        // don't do if you run a local elastic search
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(userName, passWord));
+
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostName, 443, "https"))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+
+
+    }
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+        RestHighLevelClient client =createClient();
+
+        String jsonString ="{ \"foo\" :\"bar\" }";
+        IndexRequest indexRequest =new IndexRequest("twitter", "tweets").source(jsonString, XContentType.JSON);
+        try {
+            IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+            String id =indexResponse.getId();
+            logger.info("id--->"+id);
+        } catch (IOException e) {
+            logger.error("Can not create elastic search :"+e);
+        }
+
+
+    }
 }
